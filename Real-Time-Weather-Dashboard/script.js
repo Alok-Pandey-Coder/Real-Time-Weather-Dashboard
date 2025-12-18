c87ad0b5b0460a89aabd9d2e5ca548fd41802f08
Alok-Pandey-Coder <alokumarpandey76@gmail.com>
2025-12-18 20:44:24 +0530

    Update script.js

--- Real-Time-Weather-Dashboard/script.js
+++ Real-Time-Weather-Dashboard/script.js
@@ -1,1 +1,72 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-messag");
+
+  const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
+
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    // it may throw an error
+    // server/database is always in another continent
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
+  async function fetchWeatherData(city) {
+    //gets the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
+
+    if (!response.ok) {
+      throw new Error(" City Not found");
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData(data) {
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature: ${main.temp}°C`;
+    descriptionDisplay.textContent = `Condition: ${weather[0].description}`;
+
+    const condition = weather[0].main; 
+    
+    document.body.classList.remove("clear-bg", "clouds-bg", "rain-bg");
+
+    if (condition === "Clear") {
+        document.body.classList.add("clear-bg");
+    } else if (condition === "Clouds") {
+        document.body.classList.add("clouds-bg");
+    } else if (condition === "Rain" || condition === "Drizzle") {
+        document.body.classList.add("rain-bg");
+    } else {
+        document.body.classList.add("default-bg");
+    }
+ 
+
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+}
+
+  function showError() {
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
+});
 
